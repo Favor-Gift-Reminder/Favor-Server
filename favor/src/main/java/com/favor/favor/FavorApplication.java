5d53c5da351968056334cf83eba37d858a57609b
Eun <eungi9640@gmail.com>
2023-01-06 08:52:14 +0900

    Spring boot 환경 세팅

---
+++ favor/src/main/java/com/favor/favor/FavorApplication.java
@@ -0,0 +1,13 @@
+package com.favor.favor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavorApplication.class, args);
+    }
+
+}
---
+++ favor/src/test/java/com/favor/favor/FavorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favor.favor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
