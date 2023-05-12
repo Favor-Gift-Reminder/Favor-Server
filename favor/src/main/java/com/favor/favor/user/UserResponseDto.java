ec3f0253d67ffa5b7293652750a8a61a547313d7
Eungi Jeong <eungi9640@gmail.com>
2023-05-12 16:37:50 +0900

    [Feat] JWT Access Token 권한 설정 #27
    - Swagger UI 첫 페이지, 회원가입/프로필생성/로그인 페이지는 모두 접근 가능합니다.
    - 그 외 페이지는 인가 받아야 접근 가능합니다.
    
    - 해결해야 할 점 : UNAUTHORIZED_USER 일 때 CustomResponseDto 설정 한대로 반환되지 않아서 이 부분 고쳐야 합니다.

--- favor/src/main/java/com/favor/favor/user/UserResponseDto.java
+++ favor/src/main/java/com/favor/favor/user/UserResponseDto.java
@@ -6,6 +6,7 @@ import com.favor.favor.common.enums.Role;
 import com.favor.favor.friend.FriendResponseDto;
 import com.favor.favor.gift.GiftResponseDto;
 import com.favor.favor.reminder.ReminderResponseDto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,15 +17,25 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class  UserResponseDto {
+    @ApiModelProperty(value = "1")
     private final Long userNo;
+    @ApiModelProperty(value = "favor@gmail.com")
     private String email;
+    @ApiModelProperty(value = "페이버")
     private String name;
+    @ApiModelProperty(value = "favor")
     private String userid;
+    @ApiModelProperty(value = "USER")
     private Role role;
+    @ApiModelProperty(value = "")
     private List<ReminderResponseDto> reminderList;
+    @ApiModelProperty(value = "")
     private List<GiftResponseDto> giftList;
+    @ApiModelProperty(value = "")
     private List<FriendResponseDto> friendList;
+    @ApiModelProperty(value = "")
     private List<AnniversaryResponseDto> anniversaryList;
+    @ApiModelProperty(value = "")
     private List<Favor> favorList;
 
     @Builder
