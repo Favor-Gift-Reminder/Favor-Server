b31a89741ee069edd925d76f5efc62feb732ad40
Eungi Jeong <eungi9640@gmail.com>
2023-06-03 04:50:59 +0900

    [Fix] Deprecated 코드 제거 #34
    - SecurityConfiguration 수정

--- favor/src/main/java/com/favor/favor/configuration/SecurityConfiguration.java
+++ favor/src/main/java/com/favor/favor/configuration/SecurityConfiguration.java
@@ -14,58 +14,73 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
-
+public class SecurityConfiguration   {
+//    extends WebSecurityConfigurerAdapter 삭제
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
+
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
 
-                .csrf().disable() //CSRF 방지
-                .cors()
+//    protected void configure(HttpSecurity http) throws Exception {
+    /*
+    이전 방식은 Deprecated된 WebSecurityConfigurerAdapter 클래스에서
+    protected void configure(HttpSecurity http) 메서드를 오버라이딩하여 보안 구성을 설정
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-                .and()
-                .sessionManagement().sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS)
+        /*
+        HttpSecurity 객체를 인자로 받아 보안 구성을 수행하고,
+        구성이 완료된 SecurityFilterChain 객체를 반환하는 방식으로 수정
+         */
 
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/v1/auth/**","/",
-                        "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html","/webjars/**", "/swagger/**",   // swagger
-                        "/h2-console/**",
-                        "/favicon.ico",
-                        "/users/sign-in",
-                        "/users/sign-up",
-                        "/users/profile/**").permitAll()
-                .anyRequest().authenticated()
+        httpSecurity.
+            httpBasic().disable()
+            .csrf().disable() //CSRF 방지
+            .cors()
+
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/v1/auth/**","/",
+                    "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html","/webjars/**", "/swagger/**",   // swagger
+                    "/h2-console/**",
+                    "/favicon.ico",
+                    "/users/sign-in",
+                    "/users/sign-up",
+                    "/users/profile/**").permitAll()
+            .anyRequest().authenticated()
 
 
-                .and()
-                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
-                .and()
-                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+            .and()
+            .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
 
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
+            .and()
+            .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
 
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
+        return httpSecurity.build();
     }
 }
