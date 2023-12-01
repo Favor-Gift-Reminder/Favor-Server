b768e49d2a20730990739805f760bd5a29eb7caa
Eungi Jeong <eungi9640@gmail.com>
2023-12-01 10:00:42 +0900

    [Fix] GiftSimpleDto 필드 추가, 정적 팩토리 메서드 적용

--- favor/src/main/java/com/favor/favor/gift/GiftSimpleDto.java
+++ favor/src/main/java/com/favor/favor/gift/GiftSimpleDto.java
@@ -1,7 +1,6 @@
 package com.favor.favor.gift;
 
 import com.favor.favor.photo.GiftPhoto;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,19 +8,33 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
-@Builder
 public class GiftSimpleDto {
     private Long giftNo;
     private String giftName;
     private LocalDate giftDate;
     private List<GiftPhoto> photoList;
+    private Boolean isPinned;
+    private Boolean isGiven;
 
     @Builder
-    public GiftSimpleDto(Gift gift){
-        this.giftNo = gift.getGiftNo();
-        this.giftName = gift.getGiftName();
-        this.giftDate = gift.getGiftDate();
-        this.photoList = gift.getGiftPhotoList();
+    private GiftSimpleDto(Long giftNo, String giftName, LocalDate giftDate, List<GiftPhoto> photoList, Boolean isPinned, Boolean isGiven){
+        this.giftNo = giftNo;
+        this.giftName = giftName;
+        this.giftDate = giftDate;
+        this.photoList = photoList;
+        this.isPinned = isPinned;
+        this.isGiven = isGiven;
     }
+
+    public static GiftSimpleDto from(Gift gift){
+        return GiftSimpleDto.builder()
+                .giftNo(gift.getGiftNo())
+                .giftName(gift.getGiftName())
+                .giftDate(gift.getGiftDate())
+                .photoList(gift.getGiftPhotoList())
+                .isPinned(gift.getIsPinned())
+                .isGiven(gift.getIsGiven())
+                .build();
+    }
+
 }
